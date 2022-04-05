87d50f0a576d085690980c121f829df3628bc4a4
Alvarg27 <alvargbesne@gmail.com>
2022-04-04 20:59:33 -0500

    Update Icon.js

--- components/Icon.js
+++ components/Icon.js
@@ -9,7 +9,6 @@ import {
   FaGlassMartiniAlt,
   FaAppleAlt,
   FaCarrot,
-  FaCat,
   FaCocktail,
   FaCookieBite,
   FaFish,
@@ -53,6 +52,68 @@ export default function Icon({ iconName, template }) {
       return <FaGlassWhiskey />;
     } else if (iconName === "FaGlassMartiniAlt") {
       return <FaGlassMartiniAlt />;
+    } else if (iconName === "FaAppleAlt") {
+      return <FaAppleAlt />;
+    } else if (iconName === "FaCarrot") {
+      return <FaCarrot />;
+    } else if (iconName === "FaCocktail") {
+      return <FaCocktail />;
+    } else if (iconName === "FaFish") {
+      return <FaFish />;
+    } else if (iconName === "FaCookieBite") {
+      return <FaCookieBite />;
+    } else if (iconName === "FaGlassCheers") {
+      return <FaGlassCheers />;
+    } else if (iconName === "FaGlassMartini") {
+      return <FaGlassMartini />;
+    } else if (iconName === "FaHamburger") {
+      return <FaHamburger />;
+    } else if (iconName === "FaHotdog") {
+      return <FaHotdog />;
+    } else if (iconName === "FaHeart") {
+      return <FaHeart />;
+    } else if (iconName === "FaLeaf") {
+      return <FaLeaf />;
+    } else if (iconName === "FaLemon") {
+      return <FaLemon />;
+    } else if (iconName === "FaMortarPestle") {
+      return <FaMortarPestle />;
+    } else if (iconName === "FaMugHot") {
+      return <FaMugHot />;
+    } else if (iconName === "FaPepperHot") {
+      return <FaPepperHot />;
+    } else if (iconName === "FaPizzaSlice") {
+      return <FaPizzaSlice />;
+    } else if (iconName === "FaSeedling") {
+      return <FaSeedling />;
+    } else if (iconName === "FaStar") {
+      return <FaStar />;
+    } else if (iconName === "FaSun") {
+      return <FaSun />;
+    } else if (iconName === "FaTag") {
+      return <FaTag />;
+    } else if (iconName === "FaThermometerFull") {
+      return <FaThermometerFull />;
+    } else if (iconName === "FaThermometerEmpty") {
+      return <FaThermometerEmpty />;
+    } else if (iconName === "FaStopwatch") {
+      return <FaStopwatch />;
+    } else if (iconName === "FaTint") {
+      return <FaTint />;
+    } else if (iconName === "FaUtensilSpoon") {
+      return <FaUtensilSpoon />;
+    } else if (iconName === "FaWineGlassAlt") {
+      return <FaWineGlassAlt />;
+    } else if (iconName === "FaWineGlass") {
+      return <FaWineGlass />;
+    } else if (iconName === "FaSnowflake") {
+      return <FaSnowflake />;
+    } else if (iconName === "FaUser") {
+      return <FaUser />;
+    } else if (iconName === "FaDrumstickBite") {
+      return <FaDrumstickBite />;
+    } else if (iconName === "FaBeer") {
+      return <FaBeer />;
     }
   };
 
