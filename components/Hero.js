17d499dbfc161fbcfdb88a5244c9b408af501b3c
Alvarg27 <alvargbesne@gmail.com>
2022-04-07 16:42:32 -0500

    update

--- components/Hero.js
+++ components/Hero.js
@@ -1,11 +1,19 @@
 import React from "react";
 import styles from "../styles/Hero.module.css";
+import Image from "next/image";
 
 export default function Hero({ template }) {
   return (
     <div className={styles.hero}>
       <div className={styles.container}>
-        <img src="/casantonella-logo.webp" />
+        <div className={styles.imageContainer}>
+          <Image
+            className={styles.image}
+            src="/casantonella-logo.webp"
+            height="100%"
+            width="200px"
+          />
+        </div>
         <div className={styles.overlay}></div>
       </div>
     </div>
