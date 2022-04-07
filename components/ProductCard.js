374f57ba721f14de4d1695f2d657b2c28f3207fe
Alvarg27 <alvargbesne@gmail.com>
2022-04-07 13:38:20 -0500

    Update ProductCard.js

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -77,9 +77,15 @@ export default function ProductCard({ product, selectedLanguage, template }) {
             <div className={styles.productImage}>
               <Image
                 className={styles.image}
-                src={urlFor(product.mainImage).width(120).height(120).url()}
+                src={urlFor(product.mainImage).width(240).height(240).url()}
                 width="120px"
                 height="120px"
+                alt={product.titleEs}
+                placeholder="blur"
+                blurDataURL={urlFor(product.mainImage)
+                  .width(24)
+                  .height(24)
+                  .url()}
               />
             </div>
           ) : (
